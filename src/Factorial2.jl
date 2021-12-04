60706df491d840d1369ad2280834c33dc04ed016
Letícia Maria Pequeno Madureira <leticiapequeno30@gmail.com>
2021-12-04 20:27:43 -0300

    Add module

--- src/Factorial2.jl
+++ src/Factorial2.jl
@@ -1,17 +1,21 @@
-function factorial2(number::Int64)
-    fact = one(number)
-    if number%2 == 0
-        for m in 1:number
-            if m%2 == 0
-                fact *= m
-            end
-        end
-    elseif number%2 == 1
-        for m in 1:number
-            if m%2 == 1
-                fact *= m
-            end
-        end
-    end
-    return fact
-end
+ module Factorial2                                                                                
+ 
+ function factorial2(number::Int64)
+     fact = one(number)
+     if number%2 == 0
+         for m in 1:number
+             if m%2 == 0
+                 fact *= m
+             end
+         end
+     elseif number%2 == 1
+         for m in 1:number
+             if m%2 == 1
+                 fact *= m
+             end
+         end
+     end
+     return fact
+ end
+ 
+ end # module
