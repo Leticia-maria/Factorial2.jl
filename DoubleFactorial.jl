625a0dc5b425e9d2ff8e0cda8ade4c61aa38f5c3
Letícia Maria Pequeno Madureira <leticiapequeno30@gmail.com>
2021-12-04 20:04:55 -0300

    Add spaces

--- DoubleFactorial.jl
+++ DoubleFactorial.jl
@@ -1,14 +1,14 @@
 function factorial2(number::Int64)
     fact = one(number)
-    if number%2==0
+    if number%2 == 0
         for m in 1:number
             if m%2 == 0
                 fact *= m
             end
         end
-    elseif number%2==1
+    elseif number%2 == 1
         for m in 1:number
-            if m%2== 1
+            if m%2 == 1
                 fact *= m
             end
         end
